30d406e86c1ad9b606a73d67781af8815b77b232
Djuybu <you@example.commacminhduy2004@gmail.com>
2022-09-13 23:04:59 +0700

    12345

--- test/bai2.js
+++ test/bai2.js
@@ -1,24 +1,13 @@
-function numberOneTriangle() {
-  let result = "";
-  placeHolder = document.querySelector(".trianglePlace");
-  triNumber = parseInt(document.querySelector(".triangleNum").value);
-  // if (triNumber > 1 && triNumber < 10 ) {
-  for (let index = 1; index <= triNumber; index++) {
-    for (let j = 1; j <= index; j++) {
-      result += "*";
-    }
-    result += "\n";
-    // } else {
-    //     alert ('vi phạm điều kiện')
-    // }
-    console.log(result);
-    placeHolder.appendChild (result)
+
+function numberOneTriangle(starNum) {
+ let star = ""
+ for (let index = 0; index < Number(starNum); index++) {
+  for (let j=1; j<= index; j++) {
+    star += '*'
   }
-}
-  // triAngle = document.createElement('triAngle')
-            // triAngle.type = 'div' 
-            // triAngle.innerHTML = '<img src ="triangle.png">'
-            // triAngle.style = 'icon'
-            // placeHolder.appendChild (triAngle)
-            // }
-            // placeHolder.writeIn ("</br>")
+  star += '\n'
+
+ }
+console.log (star)
+  }
+numberOneTriangle (5)
